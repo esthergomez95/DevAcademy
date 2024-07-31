f8cc2afce3f2fcb3df3e4e3bf668b56b75a25611
esther <esthergomez95@hotmail.com>
2024-07-31 21:12:48 +0200

    footer

---
+++ views/templates/footer.php
@@ -0,0 +1,32 @@
+<footer class="footer">
+    <div class="footer__container">
+        <div class= "footer__content">
+            <div class="footer__logo">
+                <img src="build/img/logo.png" alt="logo">
+            </div>
+            <p class="footer__text">
+                Impulsamos tu carrera en tecnología con educación de vanguardia y recursos especializados.
+                Únete a nosotros y transforma tu futuro digital.
+            </p>
+        </div>
+        <nav class="social-networks">
+            <ul class="social-networks__list">
+                <li class="social-networks__item social-networks__item--twitter">
+                    <span><i class="fab fa-twitter"></i></span>
+                </li>
+                <li class="social-networks__item social-networks__item--instagram">
+                    <span><i class="fab fa-instagram"></i></span>
+                </li>
+                <li class="social-networks__item social-networks__item--linkedin">
+                    <span><i class="fab fa-linkedin"></i></span>
+                </li>
+            </ul>
+        </nav>
+    </div>
+
+    <div class="footer__copyright">© DevAcademy.
+        <span class ="footer__copyright--regular" >Todos los derechos reservados
+             <?php echo date ('Y'); ?>
+        </span>
+    </div>
+</footer>
--- views/templates/header.php
+++ views/templates/header.php
@@ -1,12 +1,24 @@
-<header class="header">
-    <div class="header__container">
-        <div class="header__logo"><img src="build/img/logo.png" alt="logo"></div>
-        <nav class="header__nav">
-            <a href="#">Quienes somos</a>
-            <a href="#">Eventos</a>
-            <a href="#">Cursos</a>
-            <a href="#">DevAcademy PRO</a>
+<header class="header__container">
+    <div class="head__container">
+        <div class="head__title">
+            <h1>De aprendiz a maestro: <br> Sé imparable</h1>
+        </div>
+        <nav class="head__nav">
+            <a href="#">Iniciar sesión</a>
+            <a href="#">Registrarse</a>
         </nav>
     </div>
+    <div class="nav">
+        <div class="nav__container">
+            <div class="nav__logo"><img src="build/img/logo.png" alt="logo"></div>
+            <nav class="nav__nav">
+                <a href="#">Quienes somos</a>
+                <a href="#">Eventos</a>
+                <a href="#">Cursos</a>
+                <a href="#">DevAcademy PRO</a>
+            </nav>
+        </div>
+    </div>
 </header>
+
 
